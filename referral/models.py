562cf8494dd10afb55d10907da3d1523a49c00fa
Sri Harsha <hars4646@gmail.com>
2018-01-03 14:43:17 +0530

    FIxed Foreign KEy issue

--- referral/models.py
+++ referral/models.py
@@ -34,7 +34,7 @@ class Referrer(models.Model):
     name = models.CharField(_("Name"), max_length=255, unique=True)
     description = models.TextField(_("Description"), blank=True, null=True)
     creation_date = models.DateTimeField(_("Creation date"), auto_now_add=True)
-    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), related_name='referrers', blank=True, null=True)
+    campaign = models.ForeignKey(Campaign, verbose_name=_("Campaign"), related_name='referrers', blank=True, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
         ordering = ['name']
@@ -72,7 +72,7 @@ class UserReferrerManager(models.Manager):
 
 class UserReferrer(models.Model):
     user = models.OneToOneField(User, verbose_name=_("User"), related_name='user_referrer')
-    referrer = models.ForeignKey(Referrer, verbose_name=_("Referrer"), related_name='users')
+    referrer = models.ForeignKey(Referrer, verbose_name=_("Referrer"), related_name='users', on_delete=models.DO_NOTHING)
 
     objects = UserReferrerManager()
 
